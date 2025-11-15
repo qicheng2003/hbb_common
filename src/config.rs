ff5b80da3d66183aa0d250b4c42e4b18cb29a78c
qicheng2003 <qicheng2003@126.com>
2025-11-15 20:37:32 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -109,8 +109,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["jy.qicheng2003.com"];
+pub const RS_PUB_KEY: &str = "CEVU6R8fdXGkuWSD4RLG+2WmfpNpaNUsX6EYF3e2Y18=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
